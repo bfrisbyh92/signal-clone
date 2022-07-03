d35b84a72d154627aab1c8f54db55e76e8163e65
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-07-03 15:00:34 -0400

    I changed the logo to a black signal icon, I fixed the photoURL and displayName to match my apps users information. Changed the firebase config as I learned easier ways to use firebase during this project.

--- firebase.js
+++ firebase.js
@@ -1,7 +1,25 @@
-import { initializeApp } from 'firebase/app';
-import { initializeAuth, updateProfile } from 'firebase/auth';
-import Constants from 'expo-constants';
-import {getFirestore, collection, updateDoc, getDocs } from 'firebase/firestore'
+import { initializeApp, getApps } from 'firebase/app'
+import { getStorage } from 'firebase/storage'
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+  serverTimestamp,
+  query,
+  orderBy,
+} from 'firebase/firestore'
+// import { getDoc } from 'firebase/firestore'
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCz3x0EvaqA_ywSnIH23QFC6GPdpRuhess",
@@ -12,21 +30,27 @@ const firebaseConfig = {
   appId: "1:157384369305:web:f2b9fc727c1ac83be94408"
 };
 
-// // add firebase config
-// const firebaseConfig = {
-//   apiKey: Constants.manifest.extra.apiKey,
-//   authDomain: Constants.manifest.extra.authDomain,
-//   projectId: Constants.manifest.extra.projectId,
-//   storageBucket: Constants.manifest.extra.storageBucket,
-//   messagingSenderId: Constants.manifest.extra.messagingSenderId,
-//   appId: Constants.manifest.extra.appId,
-// };
-// ^^^ This is for later. Similiar to a dot env file in regular React? ^^^ Looked important enough to save
+if (!getApps().length) initializeApp(firebaseConfig)
+// ^^^ Only want to initialize app once, improve performance. ^^^
+const db = getFirestore()
+const auth = getAuth()
+const storage = getStorage()
 
-// initialize firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-// initialize auth
-const auth = initializeAuth(app);
-
-export { auth, db, collection, updateDoc, getDocs };
+export {
+  storage,
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+  collection,
+  addDoc,
+  getFirestore,
+  onSnapshot,
+  serverTimestamp,
+  query,
+  orderBy,
+  db,
+  auth,
+}
